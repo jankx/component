41c6b8420b30e77969639899cf5a9aa0c94902eb
Puleeno Nguyen <puleeno@gmail.com>
2020-09-09 22:04:45 +0700

    Done HTML component

--- src/HTML.php
+++ src/HTML.php
@@ -10,7 +10,21 @@ class HTML extends Component
         return 'html';
     }
 
+    protected function parseProps($props)
+    {
+        $this->props = wp_parse_props($props, array(
+            'content' => '',
+        ));
+    }
+
     public function render()
     {
+        /**
+         * Sanitizes content for allowed HTML tags for post content.
+         * @link https://developer.wordpress.org/reference/functions/wp_kses_post/
+         */
+        return wp_kses_post(
+            $this->props['content']
+        );
     }
 }
