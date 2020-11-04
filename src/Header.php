56eb0833e50ab11d1ce0069514857890ca0741da
Puleeno Nguyen <puleeno@gmail.com>
2020-11-04 10:24:55 +0700

    update header hooks

--- src/Header.php
+++ src/Header.php
@@ -71,7 +71,9 @@ class Header extends Component
             <?php do_action('jankx_template_before_header_content'); ?>
             <?php jankx_open_container(); ?>
 
+                <?php do_action('jankx_component_before_header'); ?>
                 <?php echo $this->renderChildren(); ?>
+                <?php do_action('jankx_component_after_header'); ?>
 
             <?php jankx_close_container(); ?>
           <?php do_action('jankx_template_after_header_content'); ?>
